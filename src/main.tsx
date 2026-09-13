4a9453b96d3d06423785b01e4c975ad14faaca85
zinvolle <aeroaviation12@gmail.com>
2026-09-13 20:57:24 +1000

    removed test message in main

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-console.log("MAIN.TSX IS EXECUTING");
-
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
