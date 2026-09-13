0ef36579d57a40d0dff5922c7b6e0279d377d493
zinvolle <aeroaviation12@gmail.com>
2026-09-13 20:32:18 +1000

    debug main

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log("MAIN.TSX IS EXECUTING");
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
